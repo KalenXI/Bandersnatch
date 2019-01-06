8002b4a2b070361babb7f9b42aceb12cad1868e5
KalenXI <chiklit@gmail.com>
2019-01-06 18:43:12 -0500

    Initial Upload

---
+++ BandersnatchParse/Bandersnatch.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BandersnatchParse
+{
+    class Bandersnatch
+    {
+        [JsonProperty("p_sp")]
+        public bool PSp { get; set; }
+
+        [JsonProperty("p_tt")]
+        public bool PTt { get; set; }
+
+        [JsonProperty("p_8a")]
+        public bool P8A { get; set; }
+
+        [JsonProperty("p_td")]
+        public bool PTd { get; set; }
+
+        [JsonProperty("p_cs")]
+        public bool PCs { get; set; }
+
+        [JsonProperty("p_w1")]
+        public bool PW1 { get; set; }
+
+        [JsonProperty("p_2b")]
+        public bool P2B { get; set; }
+
+        [JsonProperty("p_3j")]
+        public bool P3J { get; set; }
+
+        [JsonProperty("p_pt")]
+        public bool PPt { get; set; }
+
+        [JsonProperty("p_cd")]
+        public bool PCd { get; set; }
+
+        [JsonProperty("p_cj")]
+        public bool PCj { get; set; }
+
+        [JsonProperty("p_sj")]
+        public bool PSj { get; set; }
+
+        [JsonProperty("p_sj2")]
+        public bool PSj2 { get; set; }
+
+        [JsonProperty("p_tud")]
+        public bool PTud { get; set; }
+
+        [JsonProperty("p_lsd")]
+        public bool PLsd { get; set; }
+
+        [JsonProperty("p_vh")]
+        public bool PVh { get; set; }
+
+        [JsonProperty("p_3l")]
+        public bool P3L { get; set; }
+
+        [JsonProperty("p_3s")]
+        public bool P3S { get; set; }
+
+        [JsonProperty("p_3z")]
+        public bool P3Z { get; set; }
+
+        [JsonProperty("p_ps")]
+        public string PPs { get; set; }
+
+        [JsonProperty("p_wb")]
+        public bool PWb { get; set; }
+
+        [JsonProperty("p_kd")]
+        public bool PKd { get; set; }
+
+        [JsonProperty("p_bo")]
+        public bool PBo { get; set; }
+
+        [JsonProperty("p_5v")]
+        public bool P5V { get; set; }
+
+        [JsonProperty("p_pc")]
+        public string PPc { get; set; }
+
+        [JsonProperty("p_sc")]
+        public bool PSc { get; set; }
+
+        [JsonProperty("p_ty")]
+        public bool PTy { get; set; }
+
+        [JsonProperty("p_cm")]
+        public bool PCm { get; set; }
+
+        [JsonProperty("p_pr")]
+        public bool PPr { get; set; }
+
+        [JsonProperty("p_3ad")]
+        public bool P3Ad { get; set; }
+
+        [JsonProperty("p_s3af")]
+        public bool PS3Af { get; set; }
+
+        [JsonProperty("p_nf")]
+        public bool PNf { get; set; }
+
+        [JsonProperty("p_np")]
+        public bool PNp { get; set; }
+
+        [JsonProperty("p_ne")]
+        public bool PNe { get; set; }
+
+        [JsonProperty("p_pp")]
+        public bool PPp { get; set; }
+
+        [JsonProperty("p_tp")]
+        public bool PTp { get; set; }
+
+        [JsonProperty("p_bup")]
+        public bool PBup { get; set; }
+
+        [JsonProperty("p_be")]
+        public bool PBe { get; set; }
+
+        [JsonProperty("p_pe")]
+        public bool PPe { get; set; }
+
+        [JsonProperty("p_pae")]
+        public bool PPae { get; set; }
+
+        [JsonProperty("p_te")]
+        public bool PTe { get; set; }
+
+        [JsonProperty("p_snt")]
+        public bool PSnt { get; set; }
+
+        [JsonProperty("p_8j")]
+        public bool P8J { get; set; }
+
+        [JsonProperty("p_8d")]
+        public bool P8D { get; set; }
+
+        [JsonProperty("p_8m")]
+        public bool P8M { get; set; }
+
+        [JsonProperty("p_8q")]
+        public bool P8Q { get; set; }
+
+        [JsonProperty("p_8s")]
+        public bool P8S { get; set; }
+
+        [JsonProperty("p_8v")]
+        public bool P8V { get; set; }
+
+        [JsonProperty("p_vs")]
+        public string PVs { get; set; }
+
+        [JsonProperty("p_scs")]
+        public bool PScs { get; set; }
+
+        [JsonProperty("p_3ab")]
+        public bool P3Ab { get; set; }
+
+        [JsonProperty("p_3ac")]
+        public bool P3Ac { get; set; }
+
+        [JsonProperty("p_3aj")]
+        public bool P3Aj { get; set; }
+
+        [JsonProperty("p_3ah")]
+        public bool P3Ah { get; set; }
+
+        [JsonProperty("p_3ak")]
+        public bool P3Ak { get; set; }
+
+        [JsonProperty("p_3al")]
+        public bool P3Al { get; set; }
+
+        [JsonProperty("p_3af")]
+        public bool P3Af { get; set; }
+
+        [JsonProperty("p_5h")]
+        public bool P5H { get; set; }
+
+        [JsonProperty("p_5ac")]
+        public bool P5Ac { get; set; }
+
+        [JsonProperty("p_5ag")]
+        public bool P5Ag { get; set; }
+
+        [JsonProperty("p_5ad")]
+        public bool P5Ad { get; set; }
+
+        [JsonProperty("p_6c")]
+        public bool P6C { get; set; }
+    }
+}
---
+++ BandersnatchParse/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+
+namespace BandersnatchParse
+{
+    public partial class Form1 : Form
+    {
+        JObject props;
+        int indentLevel = 0;
+        String nl = Environment.NewLine;
+        string lastCondition = "none";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Log(string text)
+        {
+            if (text != null)
+            {
+                var curLine = textBox1.GetLineFromCharIndex(this.textBox1.SelectionStart);
+                string curLineText = "";
+                if (textBox1.Lines.Count() > 0)
+                    curLineText = textBox1.Lines[curLine];
+
+                if (curLineText == "")
+                {
+                    var indent = "".PadLeft(indentLevel * 2);
+                    textBox1.AppendText(indent + text);
+                } else
+                {
+                    textBox1.AppendText(text);
+                }
+                
+            }
+        }
+
+        void Sort(JObject jObj)
+        {
+            var props = jObj.Properties().ToList();
+            foreach (var prop in props)
+            {
+                prop.Remove();
+            }
+
+            foreach (var prop in props.OrderBy(p => p.Name))
+            {
+                jObj.Add(prop);
+                if (prop.Value is JObject)
+                    Sort((JObject)prop.Value);
+            }
+        }
+
+        private JToken GetChoices(string segmentId)
+        {
+            Log("ch(" + segmentId + ") > ");
+
+            var moment = props.SelectToken("interactiveVideoMoments.momentsBySegment['" + segmentId + "'][?(@.type == 'scene:cs_bs')]");
+
+            if (moment == null) //Not found in moments list, check if actually segmentGroup
+            {
+                moment = props.SelectToken("interactiveVideoMoments.segmentGroups['" + segmentId + "']");
+                if (moment != null) //Is actually a segmentGroup
+                {
+                    EvalSegmentGroup(segmentId);
+                } else
+                {
+                    Log("END");
+                    return null;
+                }
+            }
+
+            var choices = moment.SelectToken("$.choices");
+
+            return choices;
+        }
+
+        private void EvalChoices(JToken choices)
+        {
+            if (choices == null)
+                return;
+
+            foreach (JToken choice in choices.Children())
+            {
+                string segmentId = (string)choice.SelectToken("$.segmentId");
+                string sg = (string)choice.SelectToken("$.sg");
+
+                if (segmentId != null)
+                    EvalChoices(GetChoices(segmentId));
+
+                if (sg != null)
+                    EvalSegmentGroup(sg);
+
+                Log("\n");
+            }
+        }
+
+        private void EvalSegmentGroup(string sg)
+        {
+            Log("sg(" + sg + ") > ");
+            var segments = props.SelectToken("interactiveVideoMoments.segmentGroups['" + sg + "']");
+
+            foreach (var segment in segments.Children())
+            {
+                if (segment.SelectToken("segmentGroup") != null)
+                {
+                    EvalSegmentGroup((string)segment.SelectToken("segmentGroup"));
+                } else if (segment.SelectToken("segment") != null) {
+                    foreach (string subSegment in segment.SelectToken("segment"))
+                    {
+                        EvalChoices(subSegment);
+                    }
+                } else
+                {
+                    EvalChoices(GetChoices((string)segment));
+                }
+                
+            }
+        }
+
+        private void UnwrapPreconditions(JToken preconditions, int depth = 2)
+        {
+            if (preconditions != null)
+            {            
+                foreach (var precondition in preconditions.Children())
+                {
+                    indentLevel = depth;
+                    if (precondition.Count() > 1)
+                    {
+                        Log(":" + nl);
+                        UnwrapPreconditions(precondition, depth+1);
+                    }
+                    else
+                    {
+                        if (precondition.ToString() != "persistentState")
+                        {
+                            lastCondition = precondition.ToString();
+                            Log(precondition.ToString() + " " + precondition.Parent.Parent.First.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ProcessMoments()
+        {
+            var moments = props.SelectToken("interactiveVideoMoments.momentsBySegment");
+            Sort((JObject)moments);
+
+            foreach (JProperty moment in moments.Children())
+            {
+                indentLevel = 0;
+                Log(moment.Name + " ");
+
+                var description = (string)props.SelectToken("interactiveVideoMoments.choicePointNavigatorMetadata.choicePointsMetadata.choicePoints." + moment.Name + ".description");
+
+                if (description != null)
+                {
+                    Log("\"" + description + "\" ");
+                }
+
+                Log("{ " + nl);
+
+                indentLevel = 1;
+
+                var persistentData = moment.Value.SelectToken("$.[?(@.type == 'scene:cs_bs')].impressionData.data.persistent");
+                if (persistentData != null)
+                    Log(persistentData.ToString().Trim(new Char[] { '{', '}' }).TrimStart().TrimEnd().Replace("\"", "") + nl + nl);
+
+                persistentData = moment.Value.SelectToken("$.[?(@.type == 'notification:playbackImpression')].impressionData.data.persistent");
+                if (persistentData != null)
+                    Log(persistentData.ToString().Trim(new Char[] { '{', '}' }).TrimStart().TrimEnd().Replace("\"", "") + nl + nl);
+
+                var preconditions = moment.Value.SelectToken("$.[?(@.type == 'notification:playbackImpression')].precondition");
+
+                if (preconditions != null)
+                {
+                    Log("req:" + nl);
+                    UnwrapPreconditions(preconditions);
+                    Log(nl + nl);
+                }
+
+                indentLevel = 1;
+
+                var choices = moment.Value.SelectTokens("$.[?(@.type == 'scene:cs_bs')].choices");
+
+                if (choices != null)
+                {
+                    foreach (var choice in choices.Children())
+                    {
+                        if (choice["segmentId"] != null)
+                            Log((string)choice["text"] + ": " + (string)choice["segmentId"] + (string)choice["sg"] + nl);
+                        if (choice["sg"] != null)
+                            Log((string)choice["text"] + ": /" + (string)choice["sg"] + "/" + nl);
+                    }
+                }
+
+                indentLevel = 0;
+
+                Log(nl + "} " + nl + nl);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextReader tr = new StreamReader(@"Bandersnatch.json");
+            string jsonString = tr.ReadToEnd();
+
+            var bandersnatch = new Bandersnatch();
+
+            props = JObject.Parse(jsonString);
+
+            Log("Going to beginning of movie...\n");
+
+            //EvalChoices(GetChoices("1A"));
+            //ProcessMoments();
+
+            var moments = props.SelectToken("interactiveVideoMoments.preconditions");
+            foreach (JProperty preconditions in moments.Children())
+            {
+                indentLevel = 0;
+                Log(preconditions.Name + " ");
+                UnwrapPreconditions(preconditions);
+                Log(nl + nl);
+            }
+
+        }
+    }
+
+    class BChoice
+    {
+    }
+}
